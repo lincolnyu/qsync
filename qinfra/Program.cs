94bab30600763d8fd1d6f45e70f40727abcefdff
Lincoln Yu <lincoln.yu@seeingmachines.com>
2018-08-30 10:31:48 +1000

    Add stub for interactive session.

--- qinfra/Program.cs
+++ qinfra/Program.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace QInfra
 {
     class Program
     {
+        static void RunInteractiveSession()
+        {
+            //TODO implement it...
+        }
+
         static void Main(string[] args)
         {
-            using (var si = new StreamReader(args[0]))
+            if (args.Contains("-i"))
             {
-                var s = si.ReadToEnd();
-                var ldr = new Loader();
-                ldr.Load(s);
+                RunInteractiveSession();
+            }
+            else if (args.Contains("-s"))
+            {
+                var pos = Array.IndexOf(args, "-s");
+                if (pos >= 0 && pos+1 < args.Length)
+                {
+                    using (var si = new StreamReader(args[pos+1]))
+                    {
+                        var s = si.ReadToEnd();
+                        var ldr = new Loader();
+                        ldr.Load(s);
+                    }
+                }
+            }
+            else
+            {
+                //TODO Print help...
             }
         }
     }
